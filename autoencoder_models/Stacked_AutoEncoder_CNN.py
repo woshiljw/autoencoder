185310c553f43d895e40f5cde225f300177ff1d7
woshiljw <3222413873@qq.com>
2018-12-05 21:04:53 +0800

    rgb image test(update)

--- autoencoder_models/Stacked_AutoEncoder_CNN.py
+++ autoencoder_models/Stacked_AutoEncoder_CNN.py
@@ -17,6 +17,23 @@ class Stacked_AutoEncoder(object):
         self.pool = tf.nn.max_pool(self.conv, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
 
         # the decoder layer
+
+        self.unpool = hidden_transfer(
+            tf.nn.conv2d_transpose(self.pool, self.weights['w2'],
+                                   self.conv.get_shape(),
+                                   [1, 2, 2, 1], padding='SAME') + self.weights['b2']
+        )
+        self.deconv = tf.nn.conv2d(
+            self.unpool, self.weights['w3'], [1, 1, 1, 1], padding='SAME') + self.weights['b3']
+
+        print('deconv: ',self.deconv.get_shape())
+        self.cost = tf.reduce_mean(
+            tf.square(self.x - self.deconv)
+        )
+        self.opt = tf.train.AdamOptimizer(0.001).minimize(self.cost)
+
+        '''
+        
         output_shape = [int(self.pool.get_shape()[0]),
                         int(self.pool.get_shape()[1]), int(self.pool.get_shape()[2]),
                         self.input_shape[3]]
@@ -26,13 +43,6 @@ class Stacked_AutoEncoder(object):
         )
         self.unpool = tf.nn.conv2d_transpose(self.deconv, self.weights['w3'], self.x.get_shape(), [1, 2, 2, 1],
                                              padding='SAME')
-
-        self.cost = tf.reduce_mean(
-            tf.square(self.x - self.unpool)
-        )
-        self.opt = tf.train.AdamOptimizer(0.001).minimize(self.cost)
-
-        '''
         '''
 
     def _initialize_weights(self):
@@ -45,7 +55,20 @@ class Stacked_AutoEncoder(object):
             tf.constant(0.1, shape=[self.filter_size[3]])
         )
 
+        all_weights['w2'] = tf.Variable(
+            tf.truncated_normal([2, 2, self.filter_size[3], self.filter_size[3]], stddev=0.1))
+        all_weights['b2'] = tf.Variable(tf.constant(0.1, shape=[self.filter_size[3]]))
+        all_weights['w3'] = tf.Variable(
+            tf.truncated_normal([self.filter_size[0], self.filter_size[1],
+                                 self.filter_size[3], self.filter_size[2]], stddev=0.1)
+        )
+        print(all_weights['w3'].shape)
+        all_weights['b3'] = tf.Variable(tf.constant(0.1, shape=[self.filter_size[2]]))
+
         # deconv_filter_size = [self.filter_size[0],self.filter_size[1],self.filter_size[3],self.filter_size[3]]
+
+        '''
+        
         all_weights['w2'] = tf.Variable(
             tf.truncated_normal(self.filter_size, stddev=0.1)
         )
@@ -56,7 +79,8 @@ class Stacked_AutoEncoder(object):
         all_weights['w3'] = tf.Variable(
             tf.truncated_normal([2, 2, self.input_shape[3], self.input_shape[3]])
         )
-
+        
+        '''
         return all_weights
 
     @property
