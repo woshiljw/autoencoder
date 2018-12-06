20d75403fe143f72b5311b690677a0e1321cd3a2
woshiljw <3222413873@qq.com>
2018-12-06 16:37:06 +0800

    add upscaling

--- autoencoder_models/Stacked_AutoEncoder_CNN.py
+++ autoencoder_models/Stacked_AutoEncoder_CNN.py
@@ -26,6 +26,7 @@ class Stacked_AutoEncoder(object):
         self.unpool = tf.image.resize_nearest_neighbor(self.pool, [32, 128])
         self.decode = hidden_transfer(tf.nn.conv2d_transpose(self.unpool,self.weights['w2'],[64,32,128,64],
                                              [1,1,1,1],padding="SAME")+self.weights['b2'])
+        self.decode = batch_norm(self.decode)
 
         self.decode =tf.nn.softmax(tf.nn.conv2d(self.decode,
                          tf.truncated_normal([1,1,64,3]),[1,1,1,1],padding='SAME'
