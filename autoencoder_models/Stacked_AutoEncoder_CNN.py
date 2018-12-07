0007ab496b83e0b176bcf2c19f87935072b2504c
woshiljw <3222413873@qq.com>
2018-12-07 12:24:44 +0800

    add fullconnect_layer

--- autoencoder_models/Stacked_AutoEncoder_CNN.py
+++ autoencoder_models/Stacked_AutoEncoder_CNN.py
@@ -1,30 +1,44 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.contrib.layers import batch_norm
-
-
+'''
 class Stacked_AutoEncoder(object):
     def __init__(self):
         pass
-    def build(self,data, name, filter_size):
+'''
+class Data(object):
+    def __init__(self, datafile, batchsize):
+        self.img = np.load(datafile)['train']
+        self.train = self.img[:4480]
+        self.train_index = np.arange(len(self.train))
+        np.random.shuffle(self.train_index)
+        self.train_data = self.train[self.train_index, :]
+        self.batchsize = batchsize
+        self.num = 0
 
-        with tf.name_scope(name) as scope:
-            input = data
-            input_shape = [-1,int(input.shape[1]),int(input.shape[2]),int(input.shape[3])]
-            c1 = self._conv2d(input, name+'c1', filter_size)
-            p1 = self._maxpool2d(c1, name+'p1')
+    def batch_size(self, resize):
+        self.num += self.batchsize
+        return np.reshape(self.train_data[self.num - self.batchsize:self.num], resize)
 
-            dc1 = self._deconv2d(p1, name+'dc1', kshape=filter_size[:2], n_outputs=3)
-            up1 = self._upsample(dc1, name+'up1', factor=[2, 2])
 
-            output = self._fullyConnected(up1, name+'output',output_size=input.shape[1] * input.shape[2] * input.shape[3])
-            with tf.name_scope(name+'cost'):
+def build( data, name, filter_size):
+    with tf.name_scope(name) as scope:
+        input = data
+        input_shape = [-1, int(input.shape[1]), int(input.shape[2]), int(input.shape[3])]
+        c1 = conv2d(input, name + 'c1', filter_size)
+        p1 = maxpool2d(c1, name + 'p1')
 
-                self.cost = tf.reduce_mean(
-                    tf.square(tf.subtract(tf.reshape(output,input_shape),data)))
-            return c1,p1,dc1,up1,self.cost
+        dc1 = deconv2d(p1, name + 'dc1', kshape=filter_size[:2], n_outputs=3)
+        up1 = upsample(dc1, name + 'up1', factor=[2, 2])
 
-    def _conv2d(self, input, name, kshape, strides=[1, 1, 1, 1]):
+        output = fullyConnected(up1, name + 'output', output_size=input.shape[1] * input.shape[2] * input.shape[3])
+        with tf.name_scope(name + 'cost'):
+            cost = tf.reduce_mean(
+                tf.square(tf.subtract(tf.reshape(output, input_shape), data)))
+        return c1, p1, dc1, up1, cost
+
+
+def conv2d(input, name, kshape, strides=[1, 1, 1, 1]):
         with tf.variable_scope(name):
             W = tf.get_variable(name='w_' + name,
                                 shape=kshape,
@@ -37,7 +51,7 @@ class Stacked_AutoEncoder(object):
             out = tf.nn.leaky_relu(out)
             return out
 
-    def _deconv2d(self,input, name, kshape, n_outputs, strides=[1, 1]):
+def deconv2d(input, name, kshape, n_outputs, strides=[1, 1]):
         with tf.variable_scope(name):
             out = tf.contrib.layers.conv2d_transpose(input,
                                                      num_outputs=n_outputs,
@@ -51,7 +65,7 @@ class Stacked_AutoEncoder(object):
                                                      activation_fn=tf.nn.leaky_relu)
             return out
 
-    def _maxpool2d(self,x, name, kshape=[1, 2, 2, 1], strides=[1, 2, 2, 1]):
+def maxpool2d(x, name, kshape=[1, 2, 2, 1], strides=[1, 2, 2, 1]):
         with tf.variable_scope(name):
             out = tf.nn.max_pool(x,
                                  ksize=kshape,  # size of window
@@ -60,7 +74,7 @@ class Stacked_AutoEncoder(object):
 
             return out
 
-    def _upsample(self,input, name, factor=[2, 2]):
+def upsample(input, name, factor=[2, 2]):
 
         size = [int(input.shape[1]) * factor[0], int(input.shape[2]) * factor[1]]
         with tf.variable_scope(name):
@@ -68,7 +82,7 @@ class Stacked_AutoEncoder(object):
 
             return out
 
-    def _fullyConnected(self,input, name, output_size):
+def fullyConnected(input, name, output_size):
         with tf.variable_scope(name):
             input_size = input.shape[1:]
             input_size = int(np.prod(input_size))  # get total num of cells in one input image
@@ -84,15 +98,34 @@ class Stacked_AutoEncoder(object):
             return out
 
 def test():
-    sae = Stacked_AutoEncoder()
+    data = Data('../data/data.npz', 64)
+    x = tf.placeholder(tf.float32, [None, 32, 128, 3])
     with tf.Session() as sess:
+        _, _, _, _, cost = build(x, 'buile_model', [5, 5, 3, 64])
+        opt = tf.reduce_mean(cost)
+        sess.run(tf.global_variables_initializer())
+        print(len(data.train_data))
+
+        for epoch in range(20):
+            avg_cost = 0
+            total_batch = int(len(data.train_data) / 64)
+            data.num = 0
+            for i in range(total_batch):
+                cost1, _ = sess.run((cost, opt), feed_dict={x: data.batch_size([-1, 32, 128, 3])})
+                avg_cost += cost1 / len(data.train_data) * 64
+
+            print("Epoch:{},Cost:{:.9f}".format(epoch, avg_cost))
+
+    '''with tf.Session() as sess:
         x = tf.placeholder(tf.float32, [None, 32, 128, 3])
-        _,_,_,_,cost = sae.build(x, 'buile_model',[5,5,3,64])
-        _, _, _, _,cost1 = sae.build(x, 'buile_model1',[5,5,3,64])
+        _,_,_,_,cost = build(x, 'buile_model',[5,5,3,64])
+        _, _, _, _,cost1 = build(x, 'buile_model1',[5,5,3,64])
         sess.run(tf.global_variables_initializer())
 
         asd = sess.run(cost1,feed_dict={x:np.reshape(np.load('../data/data.npz')['train'][:64],[-1,32,128,3])})
-        print(asd)
+        print(asd)'''
+
+
 
 if __name__ == '__main__':
     test()
